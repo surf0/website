881015422de937c86c78fd30f762b2c724a3baf6
Joscha von Andrian <joschavonandrian@gmail.com>
2022-08-29 02:07:49 +0200

    add single server for fixed order

--- servers.php
+++ servers.php
@@ -1,11 +1,8 @@
 <?php
-
     $url = "https://api.steampowered.com/IGameServersService/GetServerList/v1/?key=***REMOVED***&filter=addr\\62.171.171.235";
     $json = file_get_contents($url);
     $table2 = json_decode($json, true);
     $servers = $table2["response"]["servers"];
-
-    
 ?>
 
 
@@ -19,18 +16,31 @@
         </tr>
     </thead>
     <tbody>
-        <?php foreach($servers as $server): ?>
-        <tr>
-            <td><?php echo $server['name']?></td>
-            <td>
-                <?php echo $server['players']?>/<?php echo $server['max_players']?> (<?php echo $server['bots']?> bots)
-            </td>
-            <td>
-                <a href="steam://connect/<?php echo $server['addr'] ?>">
-                    Connect
-                </a>
-            </td>
-        </tr>
-        <?php endforeach; ?>
+        <?php server("Very Easy [Tier 1]","85568392925175846", $servers) ?>
+        <?php server("Easy [Tier 1-2]","85568392925187498", $servers) ?>
+        <?php server("Medium [Tier 2-3]","85568392925188366", $servers) ?>
+        <?php server("Hard [Tier 3-8]","85568392925188368", $servers) ?>
+        <?php server("All Maps [Tier 1-8]","85568392925188370", $servers) ?>
     </tbody>
 </table>
+
+<?php
+function server($name,$id, $servers) {
+    $server_key = array_search($id, array_column($servers, 'steamid'));
+    $server = $servers[$server_key];
+    ?>
+    <tr>
+        <td><?php echo $name?></td>
+        <td><?php echo $server["map"] ?></td>
+        <td>
+            <?php echo $server['players']?>/<?php echo $server['max_players']?> (<?php echo $server['bots']?> bots)
+        </td>
+        <td>
+            <a href="steam://connect/<?php echo $server['addr'] ?>">
+                Connect
+            </a>
+        </td>
+    </tr>
+    <?php
+}
+?>
