70337bb5f3908ebeecfdda1b9511142078ab94a8
Joscha von Andrian <joschavonandrian@gmail.com>
2022-08-29 12:45:36 +0200

    add footer credit

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
             </div>
+            <div class="mt-2">
+                <hr>
+                <div class="row">
+                    <div class="text-center text-body col-lg-6 offset-lg-3">
+                        <p>
+                            <small>powered by <a class="link-dark text-decoration-none" href="https://github.com/surftimer/SurfTimer-Web-Stats" target="_blank">Surftimer-Web-Stats</a></small>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
