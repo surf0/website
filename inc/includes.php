85d3789574aa41c92ec4b229d929f2c7a820070b
Joscha <joschavonandrian@gmail.com>
2023-07-26 20:24:31 +0200

    remove server query

--- inc/includes.php
+++ inc/includes.php
@@ -1,9 +1,6 @@
 <?php
-// require_once('./inc/secret.php');
 require_once('./inc/config.php');
 require_once('./inc/docker_conf.php');
 require_once("./inc/languages.php");
 require_once('./inc/database.php');
 require_once('./inc/functions.php');
-
-require_once('./inc/server_functions.php');
