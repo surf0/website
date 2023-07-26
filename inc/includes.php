2dfd2b196be65db199a16808cbe2b3b308f6bd08
Joscha <joschavonandrian@gmail.com>
2023-07-26 23:27:20 +0200

    fix includes

--- inc/includes.php
+++ inc/includes.php
@@ -1,6 +1,6 @@
 <?php
-require_once( __DIR__.'/config.php');
-require_once( __DIR__.'/docker_conf.php');
-require_once( __DIR__."/languages.php");
-require_once( __DIR__.'/database.php');
-require_once( __DIR__.'/functions.php');
+require_once( './inc/config.php');
+require_once( './inc/docker_conf.php');
+require_once( "./inc/languages.php");
+require_once( './inc/database.php');
+require_once( './inc/functions.php');
